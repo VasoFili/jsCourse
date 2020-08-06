a83b50e4beee03404ae7b7bab2a28befe0192ce4
Vasiliy Filippov <Filippoff.v@mail.ru>
2020-08-06 23:47:41 +0300

    Комментарий

---
+++ script/script.js
@@ -0,0 +1,19 @@
+alert("Hello, Aschot");
+
+
+/* console.log(3);
+console.log(2); */
+
+// Однострочный комментарий Ctrl + |
+
+/* многострочный
+комментарий */
+
+
+
+//console.log('myAge: ', myAge);
+
+{
+   var myAge = 33;
+}
+console.log('myAge: ', myAge);
