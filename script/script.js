ad156484f240928de61839d9203457fcb4b241e3
Vasiliy Filippov <Filippoff.v@mail.ru>
2020-08-07 10:43:19 +0300

    lesson02

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 300;
+let income = 'Rent';
+let addExpenses = 'Массаж, Маникюр, Маска';
+let deposit = true;
+let mission = 3000;
+let period = 12;
 
-alert("GIT is so hard for understanding");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' монгольских тугриков');
 
-console.log('Oh my GIT');
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
